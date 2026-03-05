fdc6f56367f15089cd39bc335edb7671921acf1c
Daniela Concha <daconcha@uc.cl>
2026-03-05 00:31:51 -0300

    ⭐ Batch 0

---
+++ semana-02-entorno_de_trabajo/ejemplo.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+
+mi_variable = 10
+
+
+def saludar() -> None:
+    print("¡Hola!")
+
+
+class MiClase:
+    def __init__(self, argumento: Any) -> None:
+        self.argumento = argumento
---
+++ semana-02-entorno_de_trabajo/ejemplo2.py
@@ -0,0 +1,4 @@
+mi_nombre = __name__
+
+if __name__ == "__main__":
+    print("Soy el módulo principal")
